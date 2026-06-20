36b7dcdab4f6a367dc76ae1a7c547a7be2daad63
shaikshavalimasul <shaikshavalimasul@gmail.com>
2026-06-21 00:45:12 +0530

    Initial commit - FlashCart Spring Boot project setup

---
+++ src/main/java/com/flashcart/FlashcartApplication.java
@@ -0,0 +1,13 @@
+package com.flashcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcartApplication.class, args);
+	}
+
+}

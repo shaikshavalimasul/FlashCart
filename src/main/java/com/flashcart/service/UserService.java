f4e8b220691d89d02391f2537adaec6284e8a8c9
shaikshavalimasul <shaikshavalimasul@gmail.com>
2026-07-31 08:24:30 +0530

    Implement UserDetailsService in UserService

--- src/main/java/com/flashcart/service/UserService.java
+++ src/main/java/com/flashcart/service/UserService.java
@@ -3,16 +3,39 @@ package com.flashcart.service;
 import com.flashcart.model.User;
 import com.flashcart.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.flashcart.dto.UserRegistrationRequest;
 import com.flashcart.dto.UserResponse;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(
+            String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository
+                .findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found: " + email));
+
+        return org.springframework.security.core
+                .userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
 
     public UserResponse registerUser(
             UserRegistrationRequest request) {
